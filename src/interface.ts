020243d1463b35056322d25d3486bb8a0d879a15
PoogithaJ <poogithajegakumaran@gmail.com>
2024-06-24 09:04:52 +0530

    Delete src directory

--- src/interface.ts
+++
@@ -1,9 +0,0 @@
-export interface FileMetadata {
-    id: string;
-    originalName: string;
-    data: Buffer;
-    mimeType: string;
-    size: number;
-    path: string;
-    uploadDate: Date;
-  }
